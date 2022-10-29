8a91622127281dab93f5cb0a58bb0933ed8b9a33
ArtemFed <57373162+ArtemFed@users.noreply.github.com>
2022-10-29 04:41:04 +0300

    Update main.c

--- C Files/main.c
+++ C Files/main.c
@@ -1,71 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-#define max_size 20
+// file:
+// ./exe length input output
 
-extern void task(int *A, int length, int *B);
-extern void print_arr(int *B, int length);
+// random timer:
+// ./exe length
 
-int main(int argc, char *argv[]) {
-    int length, answer = 0;
+// random & simple:
+// ./exe
 
-    if (argc == 2) {
-        length = atoi(argv[1]);
-        FILE *input = fopen("input.txt", "r");
+#define max_size 100
 
-        int A[length];
-        int B[length];
+extern int task_cmd(int length);
+extern void task_file(char *input, char *output, int length);
 
-        for (int i = 0; i < length; ++i) {
-            fscanf(input, "%d", &A[i]);
-        }
-        fclose(input);
-        
-        task(A, length, B);
-
-		FILE *output = fopen("output.txt", "w");
-        for (int i = 0; i < length; ++i) {
-            fprintf(output, "%d ", B[i]);
-        }
-        fclose(output);
-        return 0;
-    }
-
+int read_int() {
+    int length;
     printf("Input length (0 < length <= %d):", max_size);
     scanf("%d", &length);
     if (length < 1 || length > max_size) {
         printf("Incorrect length = %d\n", length);
-        return 1;
+        exit(EXIT_SUCCESS);
+    }
+    return length;
+}
+
+void task_random(int length) {
+    int sum = 0;
+    char ch;
+
+    printf("Result string: \n");
+    srand(clock());
+    for (int i = 0; i < length; ++i) {
+        // (char) 33 = '!' && (char) 64 = @
+        ch = rand() % 32 + 33;
+        printf("%c", ch);
+        if ((ch >= '1') && (ch <= '9')) {
+            sum += ((int) ch - 48);
+        }
+    }
+    printf("\nResult: %d\n", sum);
+}
+
+// task_random without print
+void task_random_lite(int length) {
+    int sum = 0;
+    char ch;
+
+    for (int j = 0; j < length; ++j) {
+        // (char) 33 = '!' && (char) 64 = @
+        ch = rand() % 32 + 33;
+        if ((ch >= '1') && (ch <= '9')) {
+            sum += ((int) ch - 48);
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int length, answer = 0, sum;
+
+    // File
+    if (argc == 4) {
+        length = atoi(argv[1]);
+        task_file(argv[2], argv[3], length);
+        return 0;
     }
 
-    int A[length];
-    int B[length];
+    // Random + Timer
+    if (argc == 2) {
+        sum = 0;
+        length = atoi(argv[1]);
+
+        time_t t_start = clock();
+        srand(clock());
+        for (int i = 0; i < 5000000; ++i) {
+            task_random_lite(length);
+        }
+        task_random(length);
+        time_t t_end = clock();
+
+        printf("\n%d ms\n", (int) difftime(t_end, t_start)) / 1000;
+        return 0;
+    }
+
+    length = read_int();
 
     printf("Random input or Direct (ans: 1, !=1):");
     scanf("%d", &answer);
 
+    // Random
     if (answer == 1) {
-        struct timespec sec;
-        clock_gettime(CLOCK_REALTIME, &sec);
-        srand(sec.tv_nsec);
-
-        printf("Result A: \n");
-        for (int i = 0; i < length; ++i) {
-            A[i] = rand() % 10;
-            printf("A[%d] = %d\n", i, *(A + i));
-        }
-        task(A, length, B);
-        print_arr(B, length);
+        task_random(length);
         return 0;
     }
 
-    printf("\n");
-    for (int i = 0; i < length; ++i) {
-        printf("A[%d] =", i);
-        scanf("%d", A + i);
-    }
-    task(A, length, B);
-    print_arr(B, length);
+    sum = task_cmd(length);
+
+    printf("\nResult: %d\n", sum);
     return 0;
 }
