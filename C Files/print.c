05da1e5c68f72ca2b601ea660cd4e1b5c583bd4a
ArtemFed <57373162+ArtemFed@users.noreply.github.com>
2022-10-31 22:24:28 +0300

    It's begun

--- C Files/print.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void print_arr(const int *B, int length) {
-    printf("\nResult B: ");
-    for (int i = 0; i < length; ++i) {
-        printf("%d ", B[i]);
-    }
-    printf("\n");
-}
--- Modification with timer/C + timer/task_timer.c
+++
@@ -1,9 +0,0 @@
-void task(const int *A, int length, int *B) {
-    if (length == 0) {
-        return;
-    }
-    B[0] = A[0];
-    for (int i = 1; i < length; ++i) {
-        B[i] = A[i] + B[i - 1];
-    }
-}
